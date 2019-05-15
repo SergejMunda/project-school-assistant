41ed075c91f8c9fca15301e5cca82a65c365f1cc
mato28 <matic.tempfer@gmail.com>
2019-05-16 00:03:47 +0200

    Add vuetify

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title>StraightAs</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
