322a631309afce9c89c324204244bc8b65e83eee
SERGEJ MUNDA <sm6808@student.uni-lj.si>
2019-05-16 00:27:51 +0200

    dodan prijavni obrazec

---
+++ src/client/src/views/PrijavniObrazec.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-form ref="form" v-model="valid">
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        :rules="nameRules"
+        label="Name"
+        required
+      ></v-text-field>
+  
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+  
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        @click="prijava"
+      >
+        Prijavi se
+      </v-btn>
+    </v-form>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+        valid: true,
+        name: '',
+        nameRules: [
+            v => !!v || 'Name is required',
+            v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+        ],
+        email: '',
+        emailRules: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+/.test(v) || 'E-mail must be valid'
+        ],
+        }),
+
+        methods: {
+        prijava () {
+            if (this.$refs.form.prijava()) {
+            this.snackbar = true
+            }
+        }
+    }
+  }
+</script>
