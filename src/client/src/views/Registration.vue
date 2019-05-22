620e0e2672297cd6394dfc99a04267040cbf9d4f
bcerni <bc7608@student.uni-lj.si>
2019-05-22 20:51:14 +0200

    login #15

---
+++ src/client/src/views/Registration.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="registration">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <div class="card mb-5">
+            <div class="card-body">
+              <div class="form-group mb-3">
+                <input type="email" name="" class="form-control" placeholder="Enter email">
+              </div>
+              <div class="form-group mb-3">
+                <input type="password" name="" class="form-control" placeholder="Enter password">
+              </div>
+              <div class="form-group mb-3">
+                <input type="password" name="" class="form-control" placeholder="Reenter password">
+              </div>
+              <button class="btn btn-primary">Registracija</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
