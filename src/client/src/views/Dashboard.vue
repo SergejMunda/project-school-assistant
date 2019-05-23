9ca81e426bc3cd58a39b606dd38a942089bec8c0
matic tempfer <mt1484@student.uni-lj.si>
2019-05-23 22:38:09 +0000

    Revert "edit home page (pull request #24)"
    This reverts pull request #24.
    
    > edit home page

--- src/client/src/views/Dashboard.vue
+++ src/client/src/views/Dashboard.vue
@@ -1,28 +1,60 @@
 <template>
-  <v-container>
-    <v-layout align-start justify-start row wrap fill-height>
-      <v-flex xs12 md6>
-        <Calendar/>
-      </v-flex>
-      <v-flex md1/>
-      <v-flex xs12 md5>
-        <Todo/>
+  <v-container class="my-5">
+    <v-flex xs2 sm4 md2>
+      <!--<v-btn color="primary" dark>Create Ad</v-btn>-->
+      <TodoPopup @todoDodan="getTodos"/>
+    </v-flex>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 md4 lg3 v-for="todo in todos" :key="todo.id">
+        <v-card class="ma-3">
+          <v-card-title>{{ todo.email }}</v-card-title>
+          <v-card-text class="grey--text">{{ todo.opis }}</v-card-text>
+          <v-card-actions>
+            <v-btn v-on:click="deleteTodo(todo.id)" color="white--grey">
+              <v-icon small left>delete</v-icon>
+              <span>Izbrisi</span>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import Todo from "../components/Todo.vue";
-import Calendar from "../components/Calendar.vue";
+import axios from "axios";
+import TodoPopup from "../components/TodoPopup.vue";
 
 export default {
   components: {
-    Todo,
-    Calendar
+    TodoPopup
   },
   data() {
-    return {};
+    return {
+      todos: []
+    };
+  },
+  mounted() {
+    this.getTodos();
+  },
+  methods: {
+    getTodos() {
+      axios
+        .get("http://localhost:3000/api/todos")
+        .then(response => {
+          //console.log(response + "ASasSasAS");
+          this.todos = response.data;
+        })
+        .catch(error => console.error(error));
+    },
+    deleteTodo(id) {
+      axios
+        .delete(`http://localhost:3000/api/todos/${id}`)
+        .then(_data => {
+          this.getTodos();
+        })
+        .catch(error => console.error(error));
+    }
   }
 };
 </script>
